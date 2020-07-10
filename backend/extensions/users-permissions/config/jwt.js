b04c41647c1fe483a16ca6c89e68638e9b6748ff
Nikolay Raskin <nikolay.raskin98@gmail.com>
2020-07-10 11:39:07 +0300

    add new instanse

--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '22f8615b-6722-41ef-9882-299682fffc81'
-};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2dd51b37-74ae-4e81-84bf-8c6bc11b4625'
+};
